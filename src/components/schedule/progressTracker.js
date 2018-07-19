32b165dc2d7858c0e874d65f22b85e3a0e282192
Jordan Hudgens <jordan@devcamp.com>
2018-07-19 13:51:24 -0600

    built progress tracker component

---
+++ src/components/schedule/progressTracker.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ProgressTracker extends Component {
+  render() {
+    return (
+      <div className="progress-tracker">
+        <label className="progress-tracker__title">Progress Tracker</label>
+        <div className="progress-tracker__percentage" /> 0%
+      </div>
+    );
+  }
+}
+export default ProgressTracker;

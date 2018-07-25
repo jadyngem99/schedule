a9fd9344a863011623f8a8ae0bd2b4ff12c020bb
Jordan Hudgens <jordan@devcamp.com>
2018-07-25 12:35:04 -0600

    line styles and arrow animations - something is wrong with arrow when clicked and add property ???

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
 
 class Arrow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      status: false
+    };
+  }
+
+  toggleArrow = function() {
+    if (this.state.status) {
+      //closes it
+      document.getElementById("arrow").classlist.remove("arrow-closed");
+    } else {
+      //open
+      document.getElementById("arrow").classlist.add("arrow-closed");
+    }
+
+    this.setState({ status: !this.state.status });
+  }.bind(this);
+
   render() {
-    return <div className={`${this.props.className} arrow`}> </div>;
+    return (
+      <a
+        id="arrow"
+        onClick={() => this.toggleArrow()}
+        className={`${this.props.className} arrow`}
+      />
+    );
   }
 }
 
